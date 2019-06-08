3418c641db09fa198838ae8e2b03b39e877a037a
Andrew Davisson <akdavisson4@gmail.com>
2019-06-08 15:27:55 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 
 def position_taken?(board, idx)
   #if board[idx] == " " || board[idx] == "" || board[idx] == nil
-  if board[idx] == "X" || board[idx] =="O"
+  if board[idx] == "X" || board[idx] == "O"
     return true
   else
     return false
